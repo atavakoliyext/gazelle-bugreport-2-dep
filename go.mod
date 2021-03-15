106b7f126601a08ed016289376e0aa2a96c23309
Ali Tavakoli <atavakoli@yext.com>
2021-03-15 16:18:27 -0400

    Initial commit.
    This module uses github.com/atavakoliyext/gazelle-bugreport-2-fork
    at v1.0.1-fork.1.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/atavakoliyext/gazelle-bugreport-2-dep
+
+go 1.16
+
+require github.com/atavakoliyext/gazelle-bugreport-2-fork v1.0.1-fork.1
---
+++ thing/thing.go
@@ -0,0 +1,12 @@
+package thing
+
+import (
+	"fmt"
+
+	"github.com/atavakoliyext/gazelle-bugreport-2-fork/thing"
+)
+
+func DoAnotherThing() {
+	thing.DoAThing()
+	fmt.Println("This function uses gazelle-bugreport-2-fork.")
+}
